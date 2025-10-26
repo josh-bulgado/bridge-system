cdbe7257271d350fd347dfa9e0a42d235e20570b
josh-bulgado <j.ll.bulgado@gmail.com>
2025-10-26 11:38:46 +0800

    client: commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignInPage } from "./features/auth/pages/SignInPage";
-import { LandingPage } from "./features/landing/pages/LandingPage";
+import RegisterPage from "./features/auth/pages/RegisterPage";
+
+
+const LandingPage = lazy(() => import("./features/landing/pages/LandingPage"));
 
 function App() {
   return (
-    <>
-      <LandingPage />
-      {/* <SignInPage /> */}
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
